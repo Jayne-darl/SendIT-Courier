0867792e7f986d7b4df7ca376e4d5ca292d98c6f
Jayne-darl <janeuchechukwu@gmail.com>
2019-04-22 17:29:00 +0100

    feat(cancel-delivery-order): user can cancel their delivery orders
    [delivers #165417082]

--- UI/js/user-order-action.js
+++ UI/js/user-order-action.js
@@ -1,12 +1,12 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("changeOrderDestination");
+let btn = document.getElementById("changeOrderDestination");
 
 // Get the <span> element that closes the modal
 var submit = document.getElementsByClassName("close")[0];
-
+let cancel = document.getElementById("cancelOrder");
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
@@ -16,7 +16,9 @@ btn.onclick = function() {
 submit.onclick = function() {
   modal.style.display = "none";
 };
-
+cancel.onclick = function() {
+  document.location.href = "user-dashboard.html";
+};
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
